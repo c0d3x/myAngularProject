6ac7456c63aeb371a6cca12ff0b1d16a853b051f
Christer Larsen <christer.larsen@gmail.com>
2016-11-04 01:16:03 +0100

    updates

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My first Angular test</h1>'
+    selector: 'pm-app',
+    template: `
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 *ngIf='pageTitle'>{{pageTitle}}</h1>
+					<div *ngIf='pageIngress'>{{pageIngress}}</div>
+				</div>
+			</div>
+
+			<pm-products></pm-products>
+		</div>
+	`
 })
-export class AppComponent { }
+export class AppComponent {
+	pageTitle: string = '';
+	pageIngress: string = '';
+}
